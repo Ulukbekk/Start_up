1cf0af1f0a4bb5b51eaf418c07a4092ce6eaa353
Ulukbekk <ulukbekmomunkulov@gmail.com>
2021-12-23 20:31:21 +0600

    change database

--- core/settings.py
+++ core/settings.py
@@ -83,27 +83,27 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'ddspn7mgp7s16t',
-#         'USER': 'vqqzcjygpalhqo',
-#         'PASSWORD': '8098ed3c74599be0c0a20d9a4e8801a9411a144d2399ff00f474f4b5d7101c49',
-#         'HOST': 'ec2-107-23-213-65.compute-1.amazonaws.com',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'start_up',
-        'USER': 'tim',
-        'PASSWORD': '1007',
-        'HOST': '127.0.0.1',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ddspn7mgp7s16t',
+        'USER': 'vqqzcjygpalhqo',
+        'PASSWORD': '8098ed3c74599be0c0a20d9a4e8801a9411a144d2399ff00f474f4b5d7101c49',
+        'HOST': 'ec2-107-23-213-65.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'start_up',
+#         'USER': 'tim',
+#         'PASSWORD': '1007',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
